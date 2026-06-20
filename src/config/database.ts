0921b2d33b4d8268d760f5f324e354420d94fc45
yousef <yousef.haggag.tech@gmail.com>
2026-06-20 09:43:28 +0300

    update database

--- src/config/database.ts
+++ src/config/database.ts
@@ -27,7 +27,7 @@ class DatabaseConnection {
   private async createPool(): Promise<sql.ConnectionPool> {
     const config: sql.config = {
       server: env.db.server,
-      port: 29500,
+      port: 9659,
       database: env.db.database,
       user: env.db.user,
       password: env.db.password,
